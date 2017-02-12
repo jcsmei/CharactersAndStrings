8be275b45206f06d1f9c51632e3446e8f921c107
jcsme <jcsme@DESKTOP-O6725QL>
2017-02-12 13:55:52 -0500

    Add project files.

---
+++ CharactersAndStrings/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace CharactersAndStrings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbDisplay = new System.Windows.Forms.RichTextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnAboutChar = new System.Windows.Forms.Button();
+            this.btnCharString = new System.Windows.Forms.Button();
+            this.btnIsLetterChar = new System.Windows.Forms.Button();
+            this.btnIndexOf = new System.Windows.Forms.Button();
+            this.btnTwoParameterSubstring = new System.Windows.Forms.Button();
+            this.btnPractice = new System.Windows.Forms.Button();
+            this.btnOneParameterSubstring = new System.Windows.Forms.Button();
+            this.btnReverseString = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbRemove = new System.Windows.Forms.Button();
+            this.btnQuiz3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbDisplay
+            // 
+            this.rtbDisplay.Location = new System.Drawing.Point(12, 291);
+            this.rtbDisplay.Name = "rtbDisplay";
+            this.rtbDisplay.Size = new System.Drawing.Size(607, 153);
+            this.rtbDisplay.TabIndex = 0;
+            this.rtbDisplay.Text = "";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(15, 47);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(604, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnAboutChar
+            // 
+            this.btnAboutChar.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAboutChar.Location = new System.Drawing.Point(15, 82);
+            this.btnAboutChar.Name = "btnAboutChar";
+            this.btnAboutChar.Size = new System.Drawing.Size(160, 55);
+            this.btnAboutChar.TabIndex = 2;
+            this.btnAboutChar.Text = "about char";
+            this.btnAboutChar.UseVisualStyleBackColor = true;
+            this.btnAboutChar.Click += new System.EventHandler(this.btnAboutChar_Click);
+            // 
+            // btnCharString
+            // 
+            this.btnCharString.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCharString.Location = new System.Drawing.Point(15, 152);
+            this.btnCharString.Name = "btnCharString";
+            this.btnCharString.Size = new System.Drawing.Size(160, 55);
+            this.btnCharString.TabIndex = 3;
+            this.btnCharString.Text = "char from string";
+            this.btnCharString.UseVisualStyleBackColor = true;
+            this.btnCharString.Click += new System.EventHandler(this.btnCharString_Click);
+            // 
+            // btnIsLetterChar
+            // 
+            this.btnIsLetterChar.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIsLetterChar.Location = new System.Drawing.Point(240, 82);
+            this.btnIsLetterChar.Name = "btnIsLetterChar";
+            this.btnIsLetterChar.Size = new System.Drawing.Size(160, 55);
+            this.btnIsLetterChar.TabIndex = 4;
+            this.btnIsLetterChar.Text = "char.IsLetter etc.";
+            this.btnIsLetterChar.UseVisualStyleBackColor = true;
+            this.btnIsLetterChar.Click += new System.EventHandler(this.btnIsLetterChar_Click);
+            // 
+            // btnIndexOf
+            // 
+            this.btnIndexOf.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIndexOf.Location = new System.Drawing.Point(240, 152);
+            this.btnIndexOf.Name = "btnIndexOf";
+            this.btnIndexOf.Size = new System.Drawing.Size(160, 55);
+            this.btnIndexOf.TabIndex = 5;
+            this.btnIndexOf.Text = "IndexOf\r\nchar or string";
+            this.btnIndexOf.UseVisualStyleBackColor = true;
+            this.btnIndexOf.Click += new System.EventHandler(this.btnIndexOf_Click);
+            // 
+            // btnTwoParameterSubstring
+            // 
+            this.btnTwoParameterSubstring.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTwoParameterSubstring.Location = new System.Drawing.Point(459, 82);
+            this.btnTwoParameterSubstring.Name = "btnTwoParameterSubstring";
+            this.btnTwoParameterSubstring.Size = new System.Drawing.Size(160, 55);
+            this.btnTwoParameterSubstring.TabIndex = 6;
+            this.btnTwoParameterSubstring.Text = "Substring\r\nTwo Parameter";
+            this.btnTwoParameterSubstring.UseVisualStyleBackColor = true;
+            this.btnTwoParameterSubstring.Click += new System.EventHandler(this.btnTwoParameterSubstring_Click);
+            // 
+            // btnPractice
+            // 
+            this.btnPractice.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPractice.Location = new System.Drawing.Point(459, 152);
+            this.btnPractice.Name = "btnPractice";
+            this.btnPractice.Size = new System.Drawing.Size(160, 55);
+            this.btnPractice.TabIndex = 7;
+            this.btnPractice.Text = "Practice";
+            this.btnPractice.UseVisualStyleBackColor = true;
+            // 
+            // btnOneParameterSubstring
+            // 
+            this.btnOneParameterSubstring.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOneParameterSubstring.Location = new System.Drawing.Point(240, 215);
+            this.btnOneParameterSubstring.Name = "btnOneParameterSubstring";
+            this.btnOneParameterSubstring.Size = new System.Drawing.Size(160, 55);
+            this.btnOneParameterSubstring.TabIndex = 8;
+            this.btnOneParameterSubstring.Text = "Substring \r\nOne Parameter";
+            this.btnOneParameterSubstring.UseVisualStyleBackColor = true;
+            this.btnOneParameterSubstring.Click += new System.EventHandler(this.btnOneParameterSubstring_Click);
+            // 
+            // btnReverseString
+            // 
+            this.btnReverseString.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReverseString.Location = new System.Drawing.Point(15, 213);
+            this.btnReverseString.Name = "btnReverseString";
+            this.btnReverseString.Size = new System.Drawing.Size(160, 55);
+            this.btnReverseString.TabIndex = 9;
+            this.btnReverseString.Text = "Reversing a string";
+            this.btnReverseString.UseVisualStyleBackColor = true;
+            this.btnReverseString.Click += new System.EventHandler(this.btnReverseString_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 19);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Type Here:";
+            // 
+            // rtbRemove
+            // 
+            this.rtbRemove.Location = new System.Drawing.Point(439, 245);
+            this.rtbRemove.Name = "rtbRemove";
+            this.rtbRemove.Size = new System.Drawing.Size(75, 23);
+            this.rtbRemove.TabIndex = 11;
+            this.rtbRemove.Text = "button1";
+            this.rtbRemove.UseVisualStyleBackColor = true;
+            this.rtbRemove.Click += new System.EventHandler(this.rtbRemove_Click);
+            // 
+            // btnQuiz3
+            // 
+            this.btnQuiz3.Location = new System.Drawing.Point(530, 245);
+            this.btnQuiz3.Name = "btnQuiz3";
+            this.btnQuiz3.Size = new System.Drawing.Size(75, 23);
+            this.btnQuiz3.TabIndex = 12;
+            this.btnQuiz3.Text = "Quiz3 #8";
+            this.btnQuiz3.UseVisualStyleBackColor = true;
+            this.btnQuiz3.Click += new System.EventHandler(this.btnQuiz3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 456);
+            this.Controls.Add(this.btnQuiz3);
+            this.Controls.Add(this.rtbRemove);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReverseString);
+            this.Controls.Add(this.btnOneParameterSubstring);
+            this.Controls.Add(this.btnPractice);
+            this.Controls.Add(this.btnTwoParameterSubstring);
+            this.Controls.Add(this.btnIndexOf);
+            this.Controls.Add(this.btnIsLetterChar);
+            this.Controls.Add(this.btnCharString);
+            this.Controls.Add(this.btnAboutChar);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.rtbDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbDisplay;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnAboutChar;
+        private System.Windows.Forms.Button btnCharString;
+        private System.Windows.Forms.Button btnIsLetterChar;
+        private System.Windows.Forms.Button btnIndexOf;
+        private System.Windows.Forms.Button btnTwoParameterSubstring;
+        private System.Windows.Forms.Button btnPractice;
+        private System.Windows.Forms.Button btnOneParameterSubstring;
+        private System.Windows.Forms.Button btnReverseString;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button rtbRemove;
+        private System.Windows.Forms.Button btnQuiz3;
+    }
+}
+
---
+++ CharactersAndStrings/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace CharactersAndStrings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAboutChar_Click(object sender, EventArgs e)
+        {
+            char x ='G'; //a char literal
+            rtbDisplay.Text = "'A' is a char literal. It is displayed as " + x;
+            rtbDisplay.AppendText("\nA char variable stores one and only one unicode character");
+            //to find out a char's unicode number
+            rtbDisplay.AppendText("\nThe unicode number of 'A' is " + Convert.ToUInt16(x).ToString());
+            //char y = 'AB'; //ERROR
+            //char z = ' '; //ERROR            
+        }
+        private void btnReverseString_Click(object sender, EventArgs e)
+        {
+            //to convert a string into an array of characters
+            char[] x = txtInput.Text.ToArray();
+            //char[] x = "Hello".ToArray(); //string literal okay, variable okay
+            string y = " ";
+            //using a FOR loop to add characters to string y
+            //for (int i=0; i<x.Length; i++)
+            //{
+            //    y += x[i] + y;
+            //}
+            //x[0] = 'X';
+            for (int i = x.Length-1; i>=0; i--)
+            {
+                y += x[i];
+            }
+            rtbDisplay.Text = "Reversing user input and you get \n" + y;
+        }
+
+        private void btnCharString_Click(object sender, EventArgs
+            e)
+        {
+            rtbDisplay.Clear();
+            string s = txtInput.Text;
+            if (s.Length > 0)
+                for (int i = 0; i < s.Length; i++)
+                    //use indec t access a character in a string
+                    rtbDisplay.AppendText(s[i] + Environment.NewLine);
+            else
+            {
+                rtbDisplay.Text = "No user input, yet.";
+                txtInput.Focus();
+                return;
+            }
+            rtbDisplay.AppendText(
+                "\nWhen accessing characters, a string works like an array. But it is NOT!");
+            //because string is not mutable, a new string must be created
+            //s{0]='B'; //see what happens if this line is uncommented
+        }
+
+        private void btnIsLetterChar_Click(object sender, EventArgs e)
+        {
+            string x = txtInput.Text;
+            List<char> allLetter = new List<char>();
+            List<char> allDigits = new List<char>();
+            List<char> allLetterDigits = new List<char>();
+            List<char> allUpper = new List<char>();
+            List<char> allLower = new List<char>();
+            List<char> allPunctuation = new List<char>();
+            for (int i=0; i< x.Length; i++)
+            {
+                char current = x[i];
+                if (char.IsLetter(current)) allLetter.Add(current);
+                if (char.IsDigit(current)) allDigits.Add(current);
+                if (char.IsLetterOrDigit(current)) allLetterDigits.Add(current);
+                if (char.IsUpper(current)) allUpper.Add(current);
+                if (char.IsLower(current)) allLower.Add(current);
+                if (char.IsPunctuation(current)) allPunctuation.Add(current);
+            }
+            rtbDisplay.Text = "Retreiving from " + x +
+                "\nall Letters: " + string.Concat(allLetter) +
+                "\nall digits: " + string.Concat(allDigits) +
+                "\nall letters and digits: " + string.Concat(allLetterDigits) +
+                "\nall upper case letters: " + string.Concat(allUpper) +
+                "\nall lower case letters: " + string.Concat(allLower) +
+                "\nall punctuation: " + string.Concat(allPunctuation);
+
+        }
+    
+
+        private void btnOneParameterSubstring_Click(object sender, EventArgs e)
+        {
+            if (txtInput.Text.Length == 0)
+            {
+                return;
+            }
+            string x = txtInput.Text;
+            string userInput = Interaction.InputBox(
+                "please type an integer number for the substring's index");
+            int i; //to store the index
+            if (int.TryParse(userInput, out i))
+            {
+                rtbDisplay.Text = Environment.NewLine + "you have tyed " + userInput + " for your substring's index: ";
+                if (i < x.Length)
+                {
+                    rtbDisplay.AppendText(
+                        Environment.NewLine + "here is your substring: " + x.Substring(i));
+                }
+                else
+                {
+                    rtbDisplay.AppendText(
+                        Environment.NewLine + i.ToString() + " is out of range. Try a smaller number.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("That's not a legitimate input. Try again.");
+            }
+        }
+
+        private void btnTwoParameterSubstring_Click(object sender, EventArgs e)
+        {
+            rtbDisplay.Clear();
+            string x = txtInput.Text;
+            string userInput = Interaction.InputBox(@"Now type two integers separated by a coma:
+            the first integer for the substring's index, 
+the second integer for the substring's length");
+            string[] stringArray = userInput.Split(',');
+            int i, n;
+            if (int.TryParse(stringArray[0], out i) && int.TryParse(stringArray[1], out n))
+            {
+                rtbDisplay.Text = "you have selected " + i.ToString() + " for the substring's index and " +
+                    n.ToString() + " for its total number of characters. Here is your substring: \n";
+                if (i + n <= x.Length)
+                {
+                    rtbDisplay.AppendText(Environment.NewLine + x.Substring(i, n));
+                }
+                else
+                {
+                    rtbDisplay.AppendText(Environment.NewLine + "out of range. Try again.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("That's not a legitimate input. Try again");
+            }
+        }
+
+        private void btnIndexOf_Click(object sender, EventArgs e)
+        {
+            if (txtInput.Text.Length == 0)
+            { return; } //exit if no user input
+            string x = txtInput.Text;
+            rtbDisplay.Text = "User has entered the following: " + Environment.NewLine +
+                x + Environment.NewLine + Environment.NewLine;
+
+            //asking to type a string to determine if it is a substring of x
+            string searchedString = Interaction.InputBox("What substring are you looking for?");
+
+            //passing searched substring to IndexOf method
+            //int i = x.IndexOf(searchedString); //
+            x.IndexOf('i'); //accepts two parameters
+            int i = x.LastIndexOf('i');
+            if (i != -1) //searched string is not a substring if i is -1
+            {
+                rtbDisplay.Text = "\"" + searchedString + "\" is a substring of \""
+                    + x + "\" and its index is " + 1.ToString() + ".";
+            }
+        
+            else
+            {
+                rtbDisplay.Text = "\"" + searchedString 
+                    + "\" is not a substring of \"" + x + "\".";
+            }
+            //use substring method with one parameter
+        }
+
+        private void rtbRemove_Click(object sender, EventArgs e)
+        {
+            //string quoteEmmerson = "ssaasdsdasdasdsd2 * 1 * sss";
+            //char X = 'x';
+            //foreach (char ch in x)
+            //{
+            //    if (!char.IsDigit(ch))
+            //    X += c;
+            //}
+            //rtbDisplay.Text = x + " with digits removed \n" + newX;
+
+        }
+
+        private void btnQuiz3_Click(object sender, EventArgs e)
+        {
+            string quoteEmerson = "For every mnute you remain angry, you give up sity second of peace of mind.";
+            int totalLetters = 0;
+            for (int x =0; x < quoteEmerson.Length; x++)
+            {
+                if (char.IsLetter(quoteEmerson[x]))
+                {
+                    totalLetters++;
+                }
+                //foreach (char x in quoteEmerson)
+                //{
+                //    if (char.IsLetter(x))
+                //        totalLetters++;
+                //}
+                
+            }
+            MessageBox.Show("There are " + totalLetters.ToString() + " letters.");
+        }
+    }
+}
